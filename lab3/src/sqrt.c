7d2c55897ce24f8314f3b912532f475d9a03a5ba
miguelJalube <miguelJalube>
2024-04-09 11:28:56 +0200

    Avancement lab3

--- lab3/src/sqrt.c
+++ lab3/src/sqrt.c
@@ -1,9 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Non optimized
-int custom_sqrt(int number){
-    int i = 0;
+// Func from bit hacks
+unsigned int log2_cust(unsigned int v){
+    const unsigned int b[] = {0x2, 0xC, 0xF0, 0xFF00, 0xFFFF0000};
+    const unsigned int S[] = {1, 2, 4, 8, 16};
+    int i;
+
+    register unsigned int r = 0; // result of log2(v) will go here
+    for (i = 4; i >= 0; i--) // unroll for speed...
+    {
+        if (v & b[i])
+        {
+            v >>= S[i];
+            r |= S[i];
+        } 
+    }
+
+    return r;
+}
+
+// optimized
+unsigned int custom_sqrt_opt(unsigned int number){
+    unsigned int i = 0;
+    for(; i < (log2_cust(number) >> 1); ++i){
+        if(i * i == number){
+            break;
+        }
+    }
+
+    if(i == (log2_cust(number) >> 1)){
+        i = -1;
+    }
+
+    return i;
+}
+
+// non optimized
+unsigned custom_sqrt(unsigned number){
+    unsigned i = 0;
     for(; i < number/2; ++i){
         if(i * i == number){
             break;
@@ -24,7 +59,8 @@ int main(int argc, char* argv[]){
         return 1;
     }
 
-    int nbr = atoi(argv[1]);
-    printf("sqrt(%d) = %d\n", nbr, custom_sqrt(nbr));
+    unsigned nbr = atoi(argv[1]);
+    printf("non opt :    sqrt(%d) = %d\n", nbr, custom_sqrt(nbr));
+    printf("opt :        sqrt(%d) = %d\n", nbr, custom_sqrt_opt(nbr));
     return 0;
 }
